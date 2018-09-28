356c867ab992f9cef59862b049e7ba346bae3320
Nils Hedström <nils.hedstrom@if.se>
2018-09-28 10:13:25 +0200

    First checkin

---
+++ Web/Controllers/TodoController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Swashbuckle.AspNetCore.Annotations;
+using TodoActor.Interfaces;
+
+namespace Web.Controllers
+{
+    /// <summary>
+    /// Handles todo-lists
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        /// <summary>
+        /// Gets the todo items for the specified email
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns>A list of todo items</returns>
+        [HttpGet]
+        [SwaggerResponse(200, "The list of todo items was returned", typeof(IEnumerable<TodoItem>))]
+        [SwaggerResponse(404, "Could not find items for specified email")]
+        public async Task<IActionResult> GetList([FromQuery]string email,
+            CancellationToken cancellationToken)
+        {
+            ITodoActor todoActor = GetTodoActorProxy(email);
+            List<TodoItem> list = await todoActor.GetItems(cancellationToken);
+            if (list != null)
+                return Ok(list.Select(item => new TodoItem
+                {
+                    Description = item.Description,
+                    DateAdded = item.DateAdded,
+                    DateFinished = item.DateFinished,
+                    Finished = item.Finished
+                }));
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Adds a new item to the list
+        /// </summary>
+        /// <param name="email">The email</param>
+        /// <param name="request">The request</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Ok if the operation was successfull</returns>
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [SwaggerResponse(200, "The item was added")]
+        public async Task<IActionResult> AddItem([FromQuery]string email,
+            AddTodoItemRequest request, CancellationToken cancellationToken)
+        {
+            ITodoActor todoActor = GetTodoActorProxy(email);
+            await todoActor.AddItem(new TodoItem
+            {
+                Description = request.Description,
+                DateAdded = DateTime.Now,
+                DateFinished = DateTime.MinValue,
+                Finished = false
+            }, cancellationToken);
+            return Ok();
+        }
+
+        private static ITodoActor GetTodoActorProxy(string email)
+        {
+            return ActorProxy.Create<ITodoActor>(new ActorId(email));
+        }
+    }
+
+    public class AddTodoItemRequest
+    {
+        /// <summary>
+        /// The description of the item
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The Id of the item
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+
+}
---
+++ Web/Startup.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using TodoActor.Interfaces;
+
+namespace Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SchemaFilter<SchemaFilter>();
+                c.OperationFilter<OperationFilter>();
+                c.DescribeAllEnumsAsStrings();
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                c.EnableAnnotations();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+
+
+
+    public class SchemaFilter : ISchemaFilter
+    {
+        public void Apply(Schema schema, SchemaFilterContext context)
+        {
+            switch (context.SystemType.FullName)
+            {
+                case "System.DateTime":
+                    schema.Example = DateTime.Now;
+                    break;
+                case "TodoActor.Interfaces.TodoItem":
+                    schema.Example = new TodoItem
+                    {
+                        Description = "A simple task 2",
+                        DateAdded = DateTime.Now.AddDays(-7),
+                        DateFinished = DateTime.Now.AddMinutes(-10),
+                        Finished = true
+                    };
+                    break;
+                case "System.Guid":
+                    schema.Example = Guid.NewGuid();
+                    break;
+            }
+        }
+    }
+
+    public class OperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            switch (operation.OperationId)
+            {
+                case "ApiTodoGet":
+                    operation.Responses["200"].Examples = new List<TodoItem>
+                    {
+                        new TodoItem
+                        {
+                            Description = "A simple task",
+                            DateAdded = DateTime.Now.AddDays(-7),
+                            DateFinished = DateTime.Now.AddMinutes(-10),
+                            Finished = true
+                        }
+                    };
+                    break;
+                case "ApiTodoPost":
+                    operation.Parameters.Cast<NonBodyParameter>().Where(x => x.Name == "Id").ToList().ForEach(x => x.Default= Guid.NewGuid());
+                    break;
+            }
+        }
+    }
+}
